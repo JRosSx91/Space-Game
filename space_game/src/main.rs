61eb59973ed407e9dd00e0fe556eb2cfff6151da
JRosSx91 <hazee.1491@gmail.com>
2023-08-18 16:16:35 +0200

    add loop

--- space_game/src/main.rs
+++ space_game/src/main.rs
@@ -53,4 +53,26 @@ fn main() {
         let y = rand::random::<f64>() * 600.0;
         stars.push((x, y));
     }
+
+    while let Some(e) = window.next() {
+        // Dibujo del fondo estrellado
+        window.draw_2d(&e, |c, g, _| {
+            clear([0.0, 0.0, 0.0, 1.0], g); // Limpia la pantalla con negro
+
+            for &(x, y) in &stars {
+                rectangle(
+                    [1.0, 1.0, 1.0, 1.0], // Color blanco para las estrellas
+                    [x, y, 1.0, 1.0],     // Tamaño de la estrella
+                    c.transform,
+                    g,
+                );
+            }
+
+            // Dibuja la nave espacial, proyectiles y enemigos aquí
+            // ...
+        });
+
+        // Resto de la lógica del juego
+        // ...
+    }
 }
